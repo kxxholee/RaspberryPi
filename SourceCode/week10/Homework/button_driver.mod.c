9c57bf6e86777c8aff580e12ec5f6490ce7245f2
vanillaPenguin <kh0204@kakao.com>
2023-11-28 20:32:32 +0900

    week10

---
+++ SourceCode/week10/Homework/button_driver.mod.c
@@ -0,0 +1,56 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/elfnote-lto.h>
+#include <linux/export-internal.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+BUILD_LTO_INFO;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+
+static const struct modversion_info ____versions[]
+__used __section("__versions") = {
+	{ 0x7fffffff, "__gnu_mcount_nc" },
+	{ 0x7fffffff, "_printk" },
+	{ 0x7fffffff, "__aeabi_unwind_cpp_pr0" },
+	{ 0x1171558e, "gpio_to_desc" },
+	{ 0x2e4c28f8, "gpiod_get_raw_value" },
+	{ 0x51a910c0, "arm_copy_to_user" },
+	{ 0x7fffffff, "alloc_chrdev_region" },
+	{ 0x7fffffff, "__class_create" },
+	{ 0x65e8d5d2, "device_create" },
+	{ 0x7fffffff, "cdev_init" },
+	{ 0x4b8165c6, "cdev_add" },
+	{ 0x47229b5c, "gpio_request" },
+	{ 0xa25c766, "gpiod_direction_input" },
+	{ 0x7fffffff, "gpio_free" },
+	{ 0x6f5d2137, "device_destroy" },
+	{ 0x11d1d8fb, "class_destroy" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0x7fffffff, "cdev_del" },
+	{ 0x3557a220, "module_layout" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "04CAD10E98DA333928F9796");
